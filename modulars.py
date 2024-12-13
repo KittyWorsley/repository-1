dab6895cded3db8b2b80c314b4d6e482331f0752
KittyWorsley <kittyworsley@gmail.com>
2024-12-13 14:37:09 +0000

    Add files via upload

---
+++ modulars.py
@@ -0,0 +1,9 @@
+def calculator():
+    if operator=="+":
+        return 5 + 5
+    elif operator=="-":
+        return 5 - 5
+    elif operator=="*":
+        return 5*5
+    elif operator=="/":
+        return 5/4
