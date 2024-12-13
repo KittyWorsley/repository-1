cf1e1cf98376b974e126ceca27b4b45600fddef3
KittyWorsley <kittyworsley@gmail.com>
2024-12-13 14:43:04 +0000

    Add files via upload

---
+++ debugging code.py
@@ -0,0 +1,23 @@
+'''This code contains some bugs and some errors.  
+Find them and fix them
+ When you are done the program will print a message:
+Write the message at the bottom of the code before submission
+'''
+def deftract_and_rearrange(string):
+    str_1 = "".join(reversed(string[0:4].split('g'))).capitalize()
+    str_2 = "".join(string[6:13].splt('ro'))
+    str3 = join("".join(reversed(list(string[14:20]))).split(string[17]))
+    str_4 = new_func(string)
+
+    return str_1 + " " + 
+    str_2 + " " + str_3 + " " str_4
+
+def new_func(string):
+    str_4 = "".join(string[21:29].split(string[23:28]))
+    return str_4
+
+def ultra_extrct_and_rearrange(string)
+    first_transform = ultra_extrct_and_rearrange(string)
+    return first_transform
+
+print("ultra_extract_andrearrange("egthb quirock nwoGrb forijmpxv"")
