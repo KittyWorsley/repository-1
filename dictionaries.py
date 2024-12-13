9b6c6bd2306003d91e21e50526d4471898555b91
KittyWorsley <kittyworsley@gmail.com>
2024-12-13 14:12:59 +0000

    Update dictionaries.py

--- dictionaries.py
+++ dictionaries.py
@@ -1,9 +1,8 @@
 counts=dict()
-line=input("Software development is the process of creating computer programs and applications. It involves several steps, including planning, designing, coding, testing, and maintaining software. Developers use programming languages like Python, Java, and JavaScript to build software that meets user needs".
- "The goal is to create efficient, user-friendly programs that solve problems or provide entertainment. Team collaboration is often essential.")
-words=linesplit()
-print(words:,words)
-print('counting')
+ line=input("Software development is the process of creating computer programs and applications"
+  words=linesplit()
+ print(words:,words)
+    print('counting')
 for words in words
-count{word}=count.get(word ,1)+5
-print('count)
+ count{word}=count.get(word ,1)+5
+     print('count)
